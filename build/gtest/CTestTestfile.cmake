079331d37b09318b80437f007daad98a9f7f01ca
Thomas Cohn <=>
2017-06-20 19:01:03 -0400

    Did it work?

--- build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/tommy/Documents/Programming/Work/Dr_Jenkins/ros_ws/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/read_and_print_odometry/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tommy/Documents/Programming/Work/Dr_Jenkins/ros_ws/src/read_and_print_odometry
-# Build directory: /home/tommy/Documents/Programming/Work/Dr_Jenkins/ros_ws/build/read_and_print_odometry
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
